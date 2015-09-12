cb92b6f079eea784e6eeb498ad01be3b6146e27c
Giacomo Bresciani <brescia123@gmail.com>
2015-09-12 17:27:11 +0200

    added party

--- js/initMaps.js
+++ js/initMaps.js
@@ -1,61 +1,77 @@
 $(function() {
-  // initCeremonyMap();
+  initPartyMap();
   initParkingMap();
 });
 
-var initCeremonyMap = function() {
-  var map = new google.maps.Map($('.map').get(0), {
-    center: {
-      lat: 44.3098361,
-      lng: 9.3475839
-    },
-    zoom: 14,
-    zoomControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.LARGE
-    }
-  });
+var initPartyMap = function() {
 
-  var service = new google.maps.places.PlacesService(map);
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map
-  });
-  marker.setIcon('https://s3.eu-central-1.amazonaws.com/j-wedding-site/ic_wedding.png');
+    var map = new google.maps.Map($('.map-party').get(0), {
+      center: {
+        lat: 44.3515041,
+        lng: 9.3595033
+      },
+      zoom: 16
+    });
+    var infowindow = new google.maps.InfoWindow();
 
-  // Get place datails
-  service.getDetails({
-    placeId: 'ChIJzW-Scg2Y1BIRB_tIWa3Wkbg'
-  }, function(place, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      marker.position = place.geometry.location;
-      if (!place.geometry) {
-        return;
-      }
-
-      if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-      } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(16);
-      }
-
-      // Set the position of the marker using the place ID and location.
-      marker.setPlace({
-        placeId: place.place_id,
-        location: place.geometry.location
+    function addMarker(location, map, label, icon, info) {
+      var infowindow = new google.maps.InfoWindow({
+        content: '<div><strong>' + info + '</strong><br>'
       });
-      marker.setVisible(true);
-
-      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        place.formatted_address);
-      infowindow.open(map, marker);
-
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, this);
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        label: label,
+        icon: icon
       });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+
+      return marker;
     }
-  });
+
+    var service = new google.maps.places.PlacesService(map);
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map
+    });
+    marker.setIcon('https://s3.eu-central-1.amazonaws.com/j-wedding-site/ic_party.png');
+
+    // Get place datails
+    service.getDetails({
+      placeId: 'ChIJA05S_Nai1BIRggu4N02m2-M'
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        marker.position = place.geometry.location;
+        if (!place.geometry) {
+          return;
+        }
+
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(16);
+        }
+
+        // Set the position of the marker using the place ID and location.
+        marker.setPlace({
+          placeId: place.place_id,
+          location: place.geometry.location
+        });
+        marker.setVisible(true);
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+          place.formatted_address);
+        infowindow.open(map, marker);
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map, this);
+        });
+      }
+    });
 }
 
 var initParkingMap = function() {
